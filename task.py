9e6c8e0b135478fae807d19d8f19a3149de52472
katomasahiro10 <katomasahiro10@gmail.com>
2020-09-17 15:04:09 +0900

    add task

---
+++ task.py
@@ -0,0 +1,19 @@
+class task:
+    def __init__(self, cycle=1, rule_num=2):
+        self.cycle = cycle
+        self.rule_num = rule_num
+        self.step=0
+
+    @property
+    def rule(self):
+        cycle_no = self.step // self.cycle
+        return (cycle_no) % self.rule_num
+
+def main():
+    t = task()
+    for i in range(10):
+        print(t.rule)
+        t.step += 1
+
+if __name__=='__main__':
+    main()
---
+++ test_task.py
@@ -0,0 +1,38 @@
+import task
+
+def test_task():
+
+    result = ""
+    t=task.task()
+    for i in range(10):
+        result+=str(t.rule)
+        t.step += 1
+    print(result)
+    assert result=="0101010101"
+
+    result = ""
+    t=task.task(3)
+    for i in range(10):
+        result+=str(t.rule)
+        t.step += 1
+    print(result)
+    assert result=="0001110001"
+
+    result = ""
+    t=task.task(2,3)
+    for i in range(10):
+        result+=str(t.rule)
+        t.step += 1
+    print(result)
+    assert result=="0011220011"
+
+    result = ""
+    t=task.task(3,3)
+    for i in range(10):
+        result+=str(t.rule)
+        t.step += 1
+    print(result)
+    assert result=="0001112220"
+
+if __name__=='__main__':
+    test_task()
